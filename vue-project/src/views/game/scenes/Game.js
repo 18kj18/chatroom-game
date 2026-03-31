fc6237059b80c71e921c8516407dba453b927572
AnotherCoder91 <danielmiezal@gmail.com>
2026-03-31 11:18:06 +0100

    Improve movement

--- vue-project/src/views/game/scenes/Game.js
+++ vue-project/src/views/game/scenes/Game.js
@@ -9,74 +9,75 @@ export class Game extends Scene
     {
         super('Game');
         this.isChat = false;
-        this.keyW;
-        this.keyA;
-        this.keyS;
-        this.keyD;
+        this.keyA
+        this.keyS
+        this.keyD
+        this.keyW
         this.keyToggle;
+        this.keyENTER
+        this.keyESC
         this.player;
+        this.cursors;
         
     }
     create ()
     {
-        // const keys = this.input.keyboard.addKey("W");
-        this.keyW = this.input.keyboard.addKey("W");
-        this.keyA = this.input.keyboard.addKey("A");
-        this.keyS = this.input.keyboard.addKey("S");
-        this.keyD = this.input.keyboard.addKey("D");
-        this.keyToggle = this.input.keyboard.addKey("FORWARD_SLASH");
         
-
+        this.keyENTER = this.input.keyboard.addKey('ENTER',false)
+        this.keyESC = this.input.keyboard.addKey('ESC',false)
+        this.keyToggle = this.input.keyboard.addKey('FORWARD_SLASH',false)
+        this.keyA = this.input.keyboard.addKey('A',false)
+        this.keyS = this.input.keyboard.addKey('S', false)
+        this.keyD = this.input.keyboard.addKey('D', false)
+        this.keyW = this.input.keyboard.addKey('W', false)
         this.cameras.main.setBackgroundColor(0x00ff00);
 
         this.add.image(512, 384, 'green');
         
-        this.add.text(512, 200, 'Green Map', {
+        /*this.add.text(512, 200, 'Green Map', {
             fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setDepth(100).setOrigin(0.5);
-        //player = this.physics.add.sprite(200, 350, 'catbeni');
-        this.player = this.add.image(200, 350, 'catbeni');
-        //player.setCollideWorldBounds(true);
-        var cursors;
+        */
+        this.player = this.physics.add.sprite(200, 350, 'catbeni');
+        this.player.setCollideWorldBounds(true);
         
-        cursors = this.input.keyboard.createCursorKeys();
+        
+        this.cursors = this.input.keyboard.createCursorKeys();
         
         
 
         EventBus.emit('current-scene-ready', this);
     }
     update ()
-    {
-        if (this.keyToggle.isDown) {
-            this.isChat = !this.isChat;
-            console.log("buh")
-           
+    {   
+        if (this.keyESC.isDown || this.keyENTER.isDown) {
+            this.isChat = false;
+
         }
         if (this.isChat) {
             return;
         }
-        if (this.keyW.isDown) {
-            // W key
-            this.player.y -=5;
+        if (this.keyToggle.isDown) {
+            this.isChat = true;
+            
         }
-        if (this.keyD.isDown) {
-            // D key 
-            this.player.x += 5;
+        
+        if (this.keyA.isDown) {
+            this.player.x -= 5;
         }
         if (this.keyS.isDown) {
-            // S key
-            this.player.y +=5;
+            this.player.y += 5;
         }
-        if (this.keyA.isDown) {
-            // A key
-            this.player.x -=5;
-        } 
+        if (this.keyD.isDown) {
+            this.player.x += 5;
+        }
+        if (this.keyW.isDown) {
+            this.player.y -= 5;
+        }
+       
         
+               
     }
-   
-    
-    
-
 }
